b5c0852039a1927f2741c2e157da674ebca0d46b
Rhys <tejanorhys@gmail.com>
2022-07-27 02:12:46 +0800

    Added dashboard designs

--- app/src/main/java/com/example/flashcard_app/MainActivity.kt
+++ app/src/main/java/com/example/flashcard_app/MainActivity.kt
@@ -6,6 +6,7 @@ import com.example.flashcard_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        getSupportActionBar()?.hide();
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
