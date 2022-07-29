155702ba6c093f7999c601ccf847b374792c7990
14haya <22.altair.77@gmail.com>
2022-07-29 14:24:31 +0800

    added initial code for timer

--- app/src/main/java/com/example/flashcard_app/SecondActivity.kt
+++ app/src/main/java/com/example/flashcard_app/SecondActivity.kt
@@ -1,11 +1,15 @@
 package com.example.flashcard_app
+
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flashcard_app.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var timer : CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +18,30 @@ class SecondActivity : AppCompatActivity() {
 
         var score = 0
 
+        // code to display 1st question here
+
+        /*
+            unit = milliseconds
+            5000 = time left
+            1000 = interval of tick
+        */
+        timer = object : CountDownTimer(5000,1000){
+            override fun onTick(p0: Long) {
+                // displays no. of seconds left every 1 second
+                ("Time left: " + p0/1000).also { binding.textView3.text = it }
+            }
+
+            override fun onFinish() {
+                // code to display next question
+                TODO("Not yet implemented")
+            }
+
+        }
+
         binding.radioGroup.setOnCheckedChangeListener { radioGroup, _ ->
             if (binding.radioBtnCheck.isChecked) {
                 score = score + 1
+                ("Score: " + score).also { binding.textView4.text = it }
                 Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
             }
             else if (binding.radioBtnWrong.isChecked) {
@@ -25,12 +50,20 @@ class SecondActivity : AppCompatActivity() {
 
         }
 
+        binding.btnShow.setOnClickListener {
+            // code to display answer
+        }
+
         binding.btnNext.setOnClickListener {
             if (binding.radioBtnCheck.isChecked == false && binding.radioBtnWrong.isChecked == false) {
                 Toast.makeText(this, "ERROR! Must check if correct or wrong", Toast.LENGTH_SHORT).show()
             }
             else {
+
                 // code to display next question
+
+                onStop()
+                onStart()
             }
         }
 
@@ -40,7 +73,16 @@ class SecondActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
 
+    override fun onStart() {
+        super.onStart()
+        timer.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        timer.cancel()
     }
 
 }
